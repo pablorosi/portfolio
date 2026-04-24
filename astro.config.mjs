f4057d14efd9def5e1cc913a2c04ab5b507d5125
Pablo Rosi <pablorosinavarro@gmail.com>
2026-04-24 17:38:48 +0200

    feat: initial project setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,13 +6,25 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			title: 'Pablo Rosi - Portfolio',
+			favicon: '/favicon.svg',
+			components: {
+        SiteTitle: './src/components/CustomTitle.astro',
+      },
+			customCss: [
+				//Archivos de fontsource
+				'@fontsource-variable/jetbrains-mono/wght.css',
+				'@fontsource-variable/inter/wght.css',
+				'./src/styles/custom.css'
+			],
+			social: [
+				{ icon: 'github', label: 'GitHub', href: 'https://github.com/pablorosi' },
+				{ icon: 'linkedin', label: 'LinkedIn', href: 'https://www.linkedin.com/in/pablorosi/' }
+			],
 			sidebar: [
 				{
 					label: 'Guides',
 					items: [
-						// Each item here is one entry in the navigation menu.
 						{ label: 'Example Guide', slug: 'guides/example' },
 					],
 				},
